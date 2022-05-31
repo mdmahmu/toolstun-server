e23d8f81e2275ee16d66215f2860fcc97037a3d6
mmmmasum <mmm.masum@yahoo.com>
2022-05-31 14:58:25 +0600

    database connection, get all reviews, post a review done

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import 'dotenv/config';
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.glfelds.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const reviewCollection = client.db("ManufacturerWebsiteToolsTun").collection("allReviews");
+
+
+        // get all reviews
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        // post a review
+        app.post('/add_review', async (req, res) => {
+            const newReview = req.body;
+            const result = await reviewCollection.insertOne(newReview);
+            res.send({ result });
+        });
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
